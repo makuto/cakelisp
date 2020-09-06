9aeb1b3c0d8932ac77421e71989495bc01fa7995
Macoy Madson <macoymadson@gmail.com>
2020-09-06 14:15:14 -0700

    Added dependency resolver test
    It's a tricky problem because I have to guess-and-check for C invocations.
    I think my algorithm will work, but it is awfully brute force.

--- src/Main.cpp
+++ src/Main.cpp
@@ -111,6 +111,7 @@ int main(int argc, char* argv[])
 	fclose(file);
 
 	// TODO Move
+	if (false)
 	{
 		char sourceOutputName[MAX_PATH_LENGTH] = {0};
 		PrintfBuffer(sourceOutputName, "%s.cpp", filename);
