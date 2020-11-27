ec84cd0bd4270e1263ae51dca2d2b58cfaef8bfe
Macoy Madson <macoy@macoy.me>
2020-11-27 11:16:02 -0800

    Not sure what was needed to get it working
    It is now. Maybe I just needed a clean build?

--- BuildHotReloadLib.sh
+++ BuildHotReloadLib.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-./bin/cakelisp runtime/HotReloadingCodeModifier.cake runtime/TextAdventure.cake \
+./bin/cakelisp --verbose-processes \
+			   runtime/HotReloadingCodeModifier.cake runtime/TextAdventure.cake \
 	&& cd runtime && jam -j4 libGeneratedCakelisp.so
